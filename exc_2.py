90abb28ea7f3032e3b8bd979e26ab03212d7d40d
Alexandr <111227400+Alexndr45678@users.noreply.github.com>
2023-05-03 00:26:31 +0300

    1, 4 tasks on Python. 2,3 tasks on Java

--- exc_2.py
+++ exc_2.py
@@ -37,3 +37,51 @@ def isempty() -> str:
 
 isempty()
 
+# 2) Исправленный код из ДЗ(2 задача) на Java:
+# public class Task2 {
+#     public static void main(String[] args) {
+#         divideArray(new int[]{10, 11, 5, 2, 6, 34, 75, 32, 100, 90, 73});
+#         divideArray(new int[]{10, 11, 5, 2, 6, 34, 75, 32, 100, 90, 73});
+#         divideArray(null);
+#     }
+
+#     private static void divideArray(int[] intArray) {
+#         try {
+#             int d = 0;
+#             double catchedRes1 = intArray[8] / d;
+#             System.out.println("catchedRes1 = " + catchedRes1);
+#         } catch (ArrayIndexOutOfBoundsException e) {
+#             System.out.println("Catching out of bounds exception: " + e);
+#         } catch (NullPointerException e) {
+#             System.out.println("Catching null pointer exception: " + e);
+#         } catch (ArithmeticException e) {
+#             System.out.println("Catching arithmetic exception: " + e);
+#         } catch (Exception e) {
+#             System.out.println("Catching exception: " + e);
+#         }
+#     }
+# }
+
+# 3) Исправленный код из ДЗ(3 задача) на Java:
+
+# public class Task3 {
+#     public static void main(String[] args) throws Exception{
+#         try {
+#             int a = 90;
+#             int b = 3;
+#             System.out.println(a / b);
+#             printSum(23, 234);
+#             int[] abc = {1, 2};
+#             abc[3] = 9;
+#         } catch (NullPointerException ex) {
+#             System.out.println("Указатель не может указывать на null!");
+#         } catch (IndexOutOfBoundsException ex) {
+#             System.out.println("Массив выходит за пределы своего размера!");
+#         } catch (Throwable ex) {
+#             System.out.println("Что-то пошло не так...");
+#         }
+#     }
+#     public static void printSum (Integer a, Integer b) throws NullPointerException {
+#         System.out.println(a + b);
+#     }
+# }
